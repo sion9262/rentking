d1d3991d99bfee5164b28b1e9494cab5fe159771
박찬빈 <mrbluebin@bagchanbin-ui-MacBookPro.local>
2022-02-10 17:32:21 +0900

    make search

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import Footer from './Footer';
 import Header from './Header';
+import Search from './search';
 
 ReactDOM.render(
   <React.StrictMode>
     <Header />
     <App />    
+    <Search />
     <Footer />
   </React.StrictMode>,
   document.getElementById('root')
---
+++ frontend/src/search.js
@@ -0,0 +1,19 @@
+
+
+
+
+import React from "react";
+
+
+function Search() {
+  return (
+        <div className="container">
+        {/* 2 */}
+        <form>
+          <input type="text" placeholder="검색어를 입력하세요" autoFocus />
+        </form>
+      </div>
+  )
+}
+
+export default Search
