f0077aa3f280c4f2f863f87b30a268672441f8af
sion <parksion9262@gmail.com>
2022-02-18 12:01:28 +0900

    spring boot init

---
+++ backend/src/main/java/org/rentking/RentkingApplication.java
@@ -0,0 +1,13 @@
+package org.rentking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentkingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentkingApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/org/rentking/RentkingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rentking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentkingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
